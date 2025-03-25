e2c9b844eeb92814e4778badaa69ba8823316bd3
mohrem09 <amedmoh910@gmail.com>
2025-03-25 16:01:42 +0100

    Ajout du modèle utilisateur

---
+++ models/user.go
@@ -0,0 +1,8 @@
+package models
+
+type User struct {
+	ID       uint   `json:"id"`
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6"`
+}
