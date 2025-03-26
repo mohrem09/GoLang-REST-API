2888072db6f7421744d708eb3368ce3d73652ffb
mohrem09 <amedmoh910@gmail.com>
2025-03-26 12:29:35 +0100

    adding health

--- routes/auth_routes.go
+++ routes/auth_routes.go
@@ -15,6 +15,11 @@ func AuthRoutes(router *gin.Engine) {
 				"message": "Bienvenue sur l'API d'authentification!",
 			})
 		})
+		router.GET("/health", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "Bienvenue sur l'API d'authentification!",
+			})
+		})
 
 		api.POST("/register", handlers.Register)
 		api.POST("/login", handlers.Login)
